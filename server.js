c4d071963758ca278a70849b478b5e48b4c4db23
Peter Juras <p.juras@outlook.com>
2016-05-09 23:53:08 +0200

    Remove sms response validation

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const twilio = require('twilio');
 const twilioRoute = require('./lib/twilio');
 // twilio.sendTestMessage();
 
-app.post('/twilio/response', twilio.webhook(), twilioRoute.response);
+app.post('/twilio/response', twilio.webhook({ validate: false }), twilioRoute.response);
 
 app.listen(process.env.port || 3000);
