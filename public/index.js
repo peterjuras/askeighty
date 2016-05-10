818b7dbb127746f0f13e4d8a0463d387442c1dc4
Peter Juras <p.juras@outlook.com>
2016-05-10 15:56:41 +0200

    Update API demo

--- public/index.js
+++ public/index.js
@@ -1,20 +1,33 @@
 var socket = io();
 
 function sendMessage() {
+  // Sends an SMS message to the server, that is either sent directly or put into the queue.
+  // Usage:
+  // socket.emit('send', 'The message to Adrian');
   socket.emit('send', document.getElementById('message').value);
   document.getElementById('sendButton').disabled = true;
 }
 
+// Received when the position in the queue for this user has been updated. If the event is
+// received after a sendMessage call, it means that the user is in the queue.
+// Example
+// message: "1" => There is 1 user in front of you in the queue
 socket.on('queue', function(message) {
-  console.log(message);
+  document.getElementById('status').innerHTML = 'Adrian is busy and you are in the queue! There are ' +
+    message + ' people ahead of you!';
 });
+
+// Received when the current user is the active user and the message has been sent to Adrian.
+// Example
+// message: true
 socket.on('active', function(message) {
-  console.log(message);
+  document.getElementById('status').innerHTML = 'Your message was sent to Adrian! Leave this page open to see his reply!';
 });
+
+// Received when Adrian responds to a message
+// Example
+// message: "Adrian's reply" => Adrian send the text: "Adrian's reply" 
 socket.on('response', function(message) {
   document.getElementById('sendButton').disabled = false;
-  console.log(message);
-});
-socket.on('twilio-error', function(message) {
-  console.log(message);
+  document.getElementById('status').innerHTML = "Adrian's response: " + message;
 });
